f86e490856e7590bfbc4ede66cf22a32a83d3066
Attila <attila.ferenc.80@gmail.com>
2021-10-11 17:59:11 +0200

    Store updated
    removed unused methods (setters) as I made constructor
    updated main class printline with getProduct() method instead of plain text product description.

--- classstructureconstructor/src/main/java/Store.java
+++ classstructureconstructor/src/main/java/Store.java
@@ -7,16 +7,8 @@ public class Store {
         return product;
     }
 
-    public void setProduct(String product) {
-        this.product = product;
-    }
-
     public int getStock() {
         return stock;
-    }
-
-    public void setStock(int stock) {
-        this.stock = stock;
     }
 
     public Store(String product, int stock) {
